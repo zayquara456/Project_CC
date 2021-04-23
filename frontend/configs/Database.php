08a1096661325b04ae73e81e89db39ea6ba09af2
HngPhi <pigboss2507@gmail.com>
2021-04-23 17:06:03 +0700

    Hoàn thiện 70%, còn đăng nhập và chi tiết News

--- frontend/configs/Database.php
+++ frontend/configs/Database.php
@@ -2,7 +2,7 @@
 
 class Database
 {
-  const DB_DSN = 'mysql:host=localhost;dbname=test;charset=utf8';
+  const DB_DSN = 'mysql:host=localhost;dbname=project_cc;charset=utf8';
   const DB_USERNAME = 'root';
   const DB_PASSWORD = '';
 }
--- frontend/controllers/HomeController.php
+++ frontend/controllers/HomeController.php
@@ -1,14 +1,18 @@
 <?php
 require_once 'controllers/Controller.php';
 require_once 'models/Product.php';
+require_once 'models/News.php';
 
 class HomeController extends Controller {
   public function index() {
     $product_model = new Product();
     $products = $product_model->getProductInHomePage();
+    $news_model = new News();
+    $news = $news_model->getNewsNearest();
 
     $this->content = $this->render('views/homes/index.php', [
-      'products' => $products
+      'products' => $products,
+      'news' => $news,
     ]);
     require_once 'views/layouts/main.php';
   }
